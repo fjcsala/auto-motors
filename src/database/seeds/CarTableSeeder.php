8dd87939af145eec28790154c4d8c9c1d1e8127f
Fábio Sala <basis-user@fabio-sala.meuintelbras.local>
2020-01-23 12:35:41 -0300

    Ajusts in car seeds.

--- src/database/seeds/CarTableSeeder.php
+++ src/database/seeds/CarTableSeeder.php
@@ -21,5 +21,45 @@ class CarTableSeeder extends Seeder
             'color'         =>      'Branco',
             'id_branch'     =>      1
         ]);
+
+        Car :: create([
+            'chassi'        =>      '3LE.NVLND5.G9.PU0842',
+            'category'      =>      'Entrada',
+            'name'          =>      'Celta',
+            'year'          =>      2020,
+            'model'         =>      2020,
+            'color'         =>      'Preto',
+            'id_branch'     =>      2
+        ]);
+
+        Car :: create([
+            'chassi'        =>      '8AB.S78N7H.ZA.N68365',
+            'category'      =>      'Sedã Grande',
+            'name'          =>      'Civic',
+            'year'          =>      2020,
+            'model'         =>      2020,
+            'color'         =>      'Prata',
+            'id_branch'     =>      3
+        ]);
+
+        Car :: create([
+            'chassi'        =>      '1PV.AU925Y.AK.P47747',
+            'category'      =>      'Sedã Grande',
+            'name'          =>      'Jetta',
+            'year'          =>      2020,
+            'model'         =>      2020,
+            'color'         =>      'Vermelho',
+            'id_branch'     =>      4
+        ]);
+
+        Car :: create([
+            'chassi'        =>      '4X5.S453ME.52.SC5021',
+            'category'      =>      'SUV',
+            'name'          =>      'Compass',
+            'year'          =>      2020,
+            'model'         =>      2020,
+            'color'         =>      'Cinza',
+            'id_branch'     =>      5
+        ]);
     }
 }

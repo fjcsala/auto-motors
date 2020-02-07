862047ab136576afbb5fdc8170eb0b3911d46d5f
Fábio Sala <basis-user@fabio-sala.meuintelbras.local>
2020-02-07 13:30:31 -0300

    Excel file filter data and ajusts.

--- src/app/Exports/BranchExport.php
+++ src/app/Exports/BranchExport.php
@@ -4,14 +4,40 @@ namespace App\Exports;
 
 use App\Models\Branch;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class BranchExport implements FromCollection
+class BranchExport implements FromQuery, WithHeadings, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    
+    public function query()
     {
-        return Branch :: all();
+        return Branch :: where('id', '>', 0);
+    }
+    
+    public function headings(): array
+    {
+        return [
+            'RAZÃO SOCIAL',
+            'CNPJ',
+            'IE',
+            'CIDADE',
+            'UF',
+        ];
+    }
+
+    public function map($branch): array
+    {
+        return [
+            $branch->social_name,
+            $branch->cnpj,
+            $branch->ie,
+            $branch->city,
+            $branch->state,
+        ];
     }
 }
--- src/app/Exports/EmployeeExport.php
+++ src/app/Exports/EmployeeExport.php
@@ -4,14 +4,38 @@ namespace App\Exports;
 
 use App\Models\Employee;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class EmployeeExport implements FromCollection
+class EmployeeExport implements FromQuery, WithHeadings, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+
+    public function query()
     {
-        return Employee :: all();
+        return Employee :: where('id', '>', 0);
+    }
+    
+    public function headings(): array
+    {
+        return [
+            'NOME',
+            'CPF',
+            'FUNÇÃO',
+            'FILIAL',
+        ];
+    }
+
+    public function map($employee): array
+    {
+        return [
+            $employee->full_name,
+            $employee->cpf,
+            $employee->function,
+            $employee->id_branch,
+        ];
     }
 }
